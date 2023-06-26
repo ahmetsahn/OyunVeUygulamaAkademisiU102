8897fedea10a6d2d0a58ac64582ec1f6056e52d6
Ahmet Öztürk Şahin <83590669+ahmetsahn@users.noreply.github.com>
2023-06-27 00:37:16 +0300

    Alien sound eklendi.

---
+++ Assets/_Scripts/Units/Alien/AlienSound.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AlienSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip hitOnRockSound;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    public void PlayHitOnRockSound()
+    {
+        audioSource.PlayOneShot(hitOnRockSound);
+    }
+}
