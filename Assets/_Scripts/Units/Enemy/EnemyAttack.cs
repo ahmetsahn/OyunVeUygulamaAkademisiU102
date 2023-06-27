fb3a66cf7dd1f3e3325186f66c770ba3eb16f434
Ahmet Öztürk Şahin <83590669+ahmetsahn@users.noreply.github.com>
2023-06-27 18:36:58 +0300

    Düşman saldırı mantığı tamamlandı.

--- Assets/_Scripts/Units/Enemy/EnemyAttack.cs
+++ Assets/_Scripts/Units/Enemy/EnemyAttack.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     public Transform target; // Hedef nesne (sizin karakteriniz)
-    public float shootingRange = 10f; // Ateþ menzili
     public float shootingInterval = 1f; // Ateþ aralýðý
     public float bulletSpeed = 50f; // Mermi hýzý
     public GameObject bulletPrefab; // Mermi prefabý
@@ -23,8 +22,7 @@ public class EnemyAttack : MonoBehaviour
 
     private bool CanShoot()
     {
-        float distance = Vector3.Distance(transform.position, target.position);
-        return distance <= shootingRange && Time.time >= nextShotTime;
+        return Time.time >= nextShotTime;
     }
 
     private void Shoot()
@@ -32,7 +30,7 @@ public class EnemyAttack : MonoBehaviour
         Vector3 targetDirection = target.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
 
-        float randomAngle = Random.Range(-30f, 30f);
+        float randomAngle = Random.Range(-10f, 10f);
         Quaternion randomRotation = Quaternion.Euler(0f, randomAngle, 0f) * rotation;
 
         GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, randomRotation);
